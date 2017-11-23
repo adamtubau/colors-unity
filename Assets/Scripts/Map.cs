d16b675ebdd39e25398d551b4e3ff3c1b26dfd24
Unknown <nil.bellmunt@gmail.com>
2017-11-23 17:35:59 +0100

    Initialize Project

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+	public GameObject hexprefab;
+
+	//size of the map
+	int width = 20;
+	int height = 20;
+
+	float xOffset = 0.882f;
+	float zOffset = 0.764f;
+
+	void Start () {
+
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+
+				float xPos = x * xOffset;
+
+				if (y % 2 == 1) 
+				{
+					xPos += xOffset/2;
+				}
+
+				GameObject Hex_go = (GameObject)Instantiate (hexprefab, new Vector3(xPos,0,y*zOffset), Quaternion.identity); 
+
+				Hex_go.name = "Hex_" + x + "_" + y;
+				Hex_go.transform.SetParent (this.transform);
+			}
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
