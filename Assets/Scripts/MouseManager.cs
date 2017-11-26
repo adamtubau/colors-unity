f4b92cd69b9537cf22c880b3ceee33a5862df97a
Nil <nil.bellmunt@gmail.com>
2017-11-26 16:11:08 +0100

    Change color on cell touch

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -1,16 +1,33 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
 	
-	// Update is called once per frame
+
 	void Update () {
+
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		RaycastHit hitInfo;
+
+		if(Physics.Raycast(ray, out hitInfo)){
+
+			GameObject ourHitObject = hitInfo.collider.transform.gameObject;
+
+			Debug.Log (ourHitObject.name);
+
+			if (Input.GetMouseButton (0)) {
+			
+				MeshRenderer mr = ourHitObject.GetComponentInChildren<MeshRenderer> ();
+
+				mr.material.color = Color.red;
+
+			}
+
+		}
 		
 	}
 }
